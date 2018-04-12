53654220424d5d523df5163c519bcc9d245b9358
Édipo Juan <edipojs@gmail.com>
2018-04-11 23:26:51 -0400

    lint error correction

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const fizzBuzz = numero => {
+const fizzBuzz = (numero) => {
   if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
   if (numero % 3 === 0) return 'Fizz';
   if (numero % 5 === 0) return 'Buzz';
