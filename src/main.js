8c404ff4cee57b4d606d22716e809b877580bbb6
Édipo Juan <edipojs@gmail.com>
2018-04-12 00:02:18 -0400

    código sem tá totalmente testado

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const fizzBuzz = (numero) => {
+  if (numero === 0) return 0;
   if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
   if (numero % 3 === 0) return 'Fizz';
   if (numero % 5 === 0) return 'Buzz';
