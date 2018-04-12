60af70a716ea3ea46b59ceee89141c6ca498212e
Édipo Juan <edipojs@gmail.com>
2018-04-11 22:30:08 -0400

    caso de teste 10 - sucesso

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const fizzBuzz = numero => {
   if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
   if (numero % 3 === 0) return 'Fizz';
   if (numero % 5 === 0) return 'Buzz';
+  return numero;
 };
 
 export default fizzBuzz;
