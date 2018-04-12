238a43f1137e39f5984c6000c9476810ec2e860a
Édipo Juan <edipojs@gmail.com>
2018-04-11 22:09:19 -0400

    caso de teste 1 - falha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,3 +1,16 @@
+/*
+Desafio FizzBuzz
+Deve ser escrito uma lib que receba um número e:
+  Se o número for divisível por 3, no lugar do número escreva 'Fizz'
+  Se o número for divisível por 5, no lugar do número escreva 'Buzz'
+  Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz'
+  Se não for múltiplo de nada, retorna o número
+*/
+
 import { expect } from 'chai';
 
-describe('FizzBuzz', () => {});
+describe('FizzBuzz', () => {
+  it('deve retornar `Fizz` quando o número informado for multiplo de 3', () => {
+    expect(fizzBuzz(3)).to.be.equal('Fizz');
+  });
+});
